87a9e992a0686fe97ce4762f5ef5e05f01222ce7
erik_vorik <kirov_ppv@abv.bg>
2017-12-19 15:24:12 +0200

    ISpawn interface created

--- Syph.Core/Contracts/ISpawn.cs
+++ Syph.Core/Contracts/ISpawn.cs
@@ -7,5 +7,14 @@ namespace Syph.Core.Contracts
     interface ISpawn
     {
         //TODO
+        int Health { get; }
+        int Demage { get; }
+        int Armour { get; }
+        int Souls { get; }
+        int DepleteArmour { set; }
+        int DepleteHealth { set; }
+        int DepleteSouls { set; }
+
+
     }
 }
