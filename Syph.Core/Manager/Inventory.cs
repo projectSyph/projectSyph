989a9ba94f7c29352a79301a14712d9845e6efe1
IovoslavIovchev <iovoslav.i@gmail.com>
2017-12-19 16:17:17 +0200

    Added the Player class and interface

--- Syph.Core/Manager/Inventory.cs
+++ Syph.Core/Manager/Inventory.cs
@@ -5,20 +5,20 @@ using Syph.Core.Contracts;
 
 namespace Syph.Core.Manager
 {
-    class Inventory : IInventory
+    public class Inventory : IInventory
     {
         private ISpawn[][] playerInventory = new ISpawn[][]
         {
             new ISpawn[3],
-            new ISpawn[7],
-            new ISpawn[5]
+            new ISpawn[5],
+            new ISpawn[7]
         };
 
         public ISpawn[][] PlayerInventory
         {
             get { return this.playerInventory; }
 
-            protected set { this.playerInventory = value; }
+            set { this.playerInventory = value; }
         }
     }
 }
---
+++ Syph.Core/Models/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Syph.Core.Contracts;
+
+namespace Syph.Core.Models
+{
+    class Player : IPlayer
+    {
+        private ISpawn[][] playerInventory = new ISpawn[][]
+        {
+            new ISpawn[3], //Senior
+            new ISpawn[5], //Regular
+            new ISpawn[7]  //Junior
+        };
+
+        private string name;
+
+        public ISpawn[][] Inventory
+        {
+            get { return this.playerInventory; }
+
+            set { this.playerInventory = value; }
+        }
+    }
+}
