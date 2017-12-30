d66d1d5f71361b7daab2a45b22e4577093963731
erik_vorik <kirov_ppv@abv.bg>
2017-12-30 19:49:44 +0200

    Add attack command validation

--- Syph.Core/Engine/Command.cs
+++ Syph.Core/Engine/Command.cs
@@ -79,7 +79,63 @@ namespace Syph.Core.Engine
                     }
                     return true;
                 case "attack":
-                    throw new NotImplementedException();
+                    if (this.Parameters.Count != 5)
+                    {
+                        this.InvalidReason = "attack requires five parameters";
+                        return false;
+                    }
+
+                    string opponentName = Parameters[0];
+                    if (!alivePlayers.Any((p) => p.Name == opponentName))
+                    {
+                        this.InvalidReason = $"{opponentName} is not a valid player name";
+                        return false;
+                    }
+                    if (player.Name == opponentName)
+                    {
+                        this.InvalidReason = $"you can't attack yourself";
+                        return false;
+                    }
+                    IPlayer opponent = alivePlayers.First((p) => p.Name == opponentName);
+                    if (player.Team == opponent.Team)
+                    {
+                        this.InvalidReason = $"you can't attack a team member";
+                        return false;
+                    }
+
+                    //oppo monster type 
+                    string opponentMonsterType = Parameters[1];
+                    SpawnRank opponentMonsterRank;
+                    if (!(Enum.TryParse(opponentMonsterType, true, out opponentMonsterRank) && Enum.IsDefined(typeof(SpawnRank), opponentMonsterRank)))
+                    {
+                        this.InvalidReason = $"{opponentMonsterType} is not a valid monster type";
+                        return false;
+                    }
+
+                    //oppo monster type and name
+                    string opponentMonsterName = Parameters[2];
+                    if (!opponent.Inventory.Any((ISpawn monster) => monster.Rank == opponentMonsterRank && monster.Name == opponentMonsterName))
+                    {
+                        this.InvalidReason = $"you don't have a monster named {opponentMonsterName} of rank {opponentMonsterRank}";
+                        return false;
+                    }
+
+                    //my monster type
+                    string myMonsterType = Parameters[3];
+                    SpawnRank myMonsterRank;
+                    if (!(Enum.TryParse(myMonsterType, true, out myMonsterRank) && Enum.IsDefined(typeof(SpawnRank), myMonsterRank)))
+                    {
+                        this.InvalidReason = $"{myMonsterType} is not a valid monster type";
+                        return false;
+                    }
+                    //my monster type and name
+                    string myMonsterName = Parameters[4];
+                    if (!player.Inventory.Any((ISpawn monster) => monster.Rank == myMonsterRank && monster.Name == myMonsterName))
+                    {
+                        this.InvalidReason = $"you don't have a monster named {myMonsterName} of rank {myMonsterRank}";
+                        return false;
+                    }
+                    return true;
                 case "sacrifice":
                     if (this.Parameters.Count != 2)
                     {
