451021536e6e9e48e75027292ffd24c1066b233e
Stefan Veselinov <chofexx@gmail.com>
2018-02-08 13:01:27 +0200

    Console reader -> use IRender, and HinterLib to automate the input.

---
+++ Syph_V02.Core/Components/Commands/Commands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syph_V02.Core.Components.Commands
+{
+    public static class Commands
+    {
+        public static string[] CommandsList
+        {
+            get
+            {
+                return commandsList;
+            }
+        }
+
+        private  static string[] commandsList = new[]
+        {
+              "help => commands help",
+              "surrender",
+              "sacrifice <monster_type> <monster_name>",
+              "attack <opponent's_name> <monster_type> <monster_name> <my_monster_type> <my_monster_name>",
+              "inventory <team_id> <player_id>",
+              "summon <monster_type> <monster_name> <souls_for_monster>",
+              "skip",
+              "add player",
+              "Player name ",
+              "2 players game"
+        };
+
+
+    }
+}
--- Syph_V02.Core/Components/Engine/ConsoleClient/ConsoleRenderer.cs
+++ Syph_V02.Core/Components/Engine/ConsoleClient/ConsoleRenderer.cs
@@ -1,112 +1,25 @@
-﻿using Syph_V02.Core.Components.Engine.ConsoleClient.Contracts;
-using Syph_V02.Core.Components.Engine.Contracts;
-using System;
+﻿using Syph_V02.Core.Components.Engine.Contracts;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
+using HinterLib;
+using System;
+using Syph_V02.Core.Components.Commands;
 
 namespace Syph_V02.Core.Components.Engine.ConsoleClient
 {
+  
+}
     public class ConsoleRenderer :  IRenderer
     {
-        public static string[] data = new[]
-      {
-            "add",
-            "add monster",
-            "monster",
-            "Bar",
-            "Barbec",
-            "Barbecue",
-            "Batman",
-        };
-
-        public static void ClearCurrentLine()
-        {
-            var currentLine = Console.CursorTop;
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, currentLine);
-        }
-
-        public static void HandleKeyInput(StringBuilder builder, IEnumerable<string> data, ConsoleKeyInfo input)
-        {
-            var currentInput = builder.ToString();
-            if (input.Key == ConsoleKey.Backspace && currentInput.Length > 0)
-            {
-                builder.Remove(builder.Length - 1, 1);
-                ClearCurrentLine();
-
-                currentInput = currentInput.Remove(currentInput.Length - 1);
-                Console.Write(currentInput);
-            }
-            else
-            {
-                var key = input.KeyChar;
-                builder.Append(key);
-                Console.Write(key);
-            }
-        }
-
-        public  static void HandleTabInput(StringBuilder builder, IEnumerable<string> data)
-        {
-            var currentInput = builder.ToString();
-
-            var match = data
-                .FirstOrDefault(item => item != 
-                currentInput &&
-                item.StartsWith(currentInput, true, CultureInfo.InvariantCulture));
-
-            if (string.IsNullOrEmpty(match))
-            {
-                return;
-            }
-
-            ClearCurrentLine();
-            builder.Clear();
-
-            Console.Write(match);
-            builder.Append(match);
-        }
-
-
         public IEnumerable<string> Input()
         {
-            var builder = new StringBuilder();
-            var input = Console.ReadKey(intercept: true);
+            var input = Hinter.ReadHintedLine(Commands.CommandsList, d => d);
 
-            while (string.IsNullOrEmpty(input.Key.ToString()));
-            {
-
-                while (input.Key != ConsoleKey.Enter)
-                {
-                    if (input.Key == ConsoleKey.Tab)
-                    {
-                        HandleTabInput(builder, data);
-                    }
-                    else
-                    {
-                        HandleKeyInput(builder, data, input);
-                    }
-
-                    input = Console.ReadKey(intercept: true);
-                }
-
-                yield return builder.ToString();
+            while (!string.IsNullOrEmpty(input))
+            {               
+                yield return input;
+                input = Hinter.ReadHintedLine(Commands.CommandsList, d => d);
             }
-          
+            
         }
-
-        
-
-        //public void Output(IEnumerable<string> output)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //IEnumerable<string> IRenderer.Input()
-        //{
-        //    throw new NotImplementedException();
-        //}
-    }
+       
 }
